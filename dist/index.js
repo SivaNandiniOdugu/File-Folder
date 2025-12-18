7068612d680377777bc2f5c9eb1adf154546218a
SivaNandini-N220020 <n220020@rguktn.ac.in>
2025-12-18 13:07:32 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("TypeScript is Working");
+/*const user={name:"Nandini",age:10}
+
+console.log("Nandini")
+console.log(user.age)
+*/
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+let a = getUpper("nandini");
+console.log(a);
+addTwo(5);
